d00fcd1cc830ec3845275b18ff2a145b3a96c1f9
Mohammed Yaseen <mohammedyaseen@Mohammeds-MacBook-Pro.local>
2025-04-02 12:25:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+     <Card title="Hot Latina"/>
+     <Card title="Hot Latina"/>
+     <Card title="Hot Latina"/>
+     <Card title="Hot Latina"/>
+     <Card title="Hot Latina"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({title}) => {
+  return (
+    <>
+        <div className='card'>
+            <img src="" alt="" />
+            <h1>{title}</h1>
+        </div>
+    </>
+  )
+}
+
+export default Card
